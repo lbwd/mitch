ff804e07326900430041dfe776bac08c5d0cfa91
Luca Benedetti <lucaunliked@gmail.com>
2022-09-17 17:22:24 +0200

    Added imgs and slider working.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/*
+  Offici x Luca Benedetti
+  https://linktr.ee/lucaunliked
+*/
+const interval = [];
+
+window.addEventListener('load', (loadEv) => {
+  const imagesSlider = document.getElementsByClassName('images-slider')[0];
+
+  document.getElementById('arrow-left').addEventListener('mousedown', (e) => {
+    interval.push(
+      setInterval(function () {
+        imagesSlider.scrollLeft -= 3;
+      }, 10)
+    );
+  });
+
+  document.getElementById('arrow-right').addEventListener('mousedown', (e) => {
+    interval.push(
+      setInterval(function () {
+        imagesSlider.scrollLeft += 3;
+      }, 10)
+    );
+  });
+});
+
+window.addEventListener('mouseup', function (event) {
+  interval.forEach((i) => clearInterval(i));
+});
